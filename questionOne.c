337f3669029cd590797e42af9b4e7d702de0ac29
MatthewZheng <mattlzheng@gmail.com>
2017-05-01 23:22:04 -0700

    Added first solution

---
+++ questionOne.c
@@ -0,0 +1,24 @@
+/*
+ *Project Euler multiples of 3 and 5
+ *Author: Matthew Zheng
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+	int mySum = 0;
+
+	for (int i = 0; i < 100; i++){
+		if (i % 5 == 0 || i % 3 == 0){
+			mySum += i;
+			printf("%d\n", i);
+		}
+	}
+
+	printf("The sum is %d\n", mySum);
+
+	system("PAUSE");
+	return 0;
+
+}
